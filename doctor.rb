eac1b8bf0fb94c4cdc43160ba1ec3a12c788d0cc
Douglas Berkley <douglasmberkley@gmail.com>
2026-01-28 11:04:44 +0900

    morning livecode

--- doctor.rb
+++ doctor.rb
@@ -1,13 +1,34 @@
 require 'sqlite3'
 DB = SQLite3::Database.new("patients_doctors.sqlite")
 
+# Doctor model
+
 class Doctor
   def initialize(attributes = {})
-    '??'
+    @id = attributes[:id] || attributes['id']
+    @first_name = attributes[:first_name] || attributes['first_name']
+    @last_name = attributes[:last_name]|| attributes['last_name']
+    @age = attributes[:age] || attributes['age']
+    @specialty = attributes[:specialty] || attributes['specialty']
   end
 
+  def self.all
+    DB.results_as_hash = true
+    p rows = DB.execute('SELECT * FROM doctors')
+    rows.map do |row|
+      Doctor.new(row)
+    end
+  end
+
+  # celso.save
+  def save
+    # insert the values of the instance into the DB
+    DB.execute('INSERT INTO doctors (first_name) VALUES (?)', @first_name)
+  end
 
 end
 
 
-# @doctor_repository.all
+# @doctor_repository.all -> Doctor.all
+# an array of doctor instances
+p Doctor.all
---
+++ sql_basics.sql
@@ -0,0 +1,91 @@
+-- SQLite
+
+-- Read
+-- SELECT column_name FROM table_name;
+-- selecting specific column
+-- SELECT first_name, last_name FROM doctors;
+
+-- reading all of the info
+-- SELECT * FROM doctors;
+
+-- filtering
+-- SELECT * FROM doctors
+-- WHERE column_name = 'value'
+-- SELECT * FROM doctors
+-- WHERE specialty = 'Dermatology';
+-- SELECT * FROM doctors
+-- WHERE age > 50;
+-- SELECT * FROM doctors
+-- WHERE age > 50
+-- OR first_name = 'Kenji';
+-- SELECT * FROM doctors
+-- WHERE specialty LIKE '%surgery%';
+
+-- SELECT * FROM doctors
+-- ORDER BY column_name ASC;
+-- SELECT * FROM doctors
+-- WHERE specialty LIKE '%surgery%'
+-- ORDER BY last_name DESC;
+
+-- Aggregate Functions -> collection of doctors
+-- COUNT()
+-- AVG()
+-- SUM()
+-- MAX()
+-- MIN()
+-- ROUND(x, num)
+-- SELECT COUNT(*) FROM doctors;
+-- SELECT MIN(age) FROM doctors;
+-- SELECT * FROM doctors;
+-- SELECT COUNT(*) c, specialty FROM doctors
+-- GROUP BY specialty;
+
+
+-- SELECT * FROM doctors;
+
+-- Joining tables
+-- SELECT * FROM table_name
+-- JOIN other_table_name ON table.primary_key = other_table.foreign_key
+
+-- Read all the appointments with doctors infos
+-- SELECT * FROM appointments
+-- JOIN doctors ON doctors.id = appointments.doctor_id;
+
+-- Read all patients first & last names along with their appointment dates
+-- SELECT patients.first_name, patients.last_name, appointments.occurs_on
+-- FROM patients
+-- JOIN appointments ON patients.id = appointments.patient_id;
+
+
+-- Read Ryota's appointments dates along with his patients first name & last name
+-- SELECT patients.first_name, patients.last_name, appointments.occurs_on
+-- FROM patients
+-- JOIN appointments ON patients.id = appointments.patient_id
+-- JOIN doctors ON doctors.id = appointments.doctor_id
+-- WHERE doctors.first_name = 'Raphael';
+
+
+-- Create -> INSERT INTO
+-- INSERT INTO table_name (column_name, column_name)
+-- VALUES (value_for_column_one, value_for_column_two)
+
+
+INSERT INTO doctors (first_name, last_name, specialty)
+VALUES ('Doug', 'Y', 'Generalist');
+
+
+-- UPDATE table_name
+-- SET column_name = value
+-- WHERE id = 1;
+
+
+-- UPDATE doctors
+-- SET age = 20, last_name = 'Yama'
+-- WHERE id = 8;
+
+-- DELETE FROM table_name WHERE id = 2;
+
+DELETE FROM doctors WHERE id = 7;
+
+
+SELECT * FROM doctors;
